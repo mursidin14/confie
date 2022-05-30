810f8237f34f115ed79b82d29faccc8294beae16
anca <annascasmawannn@gmail.com>
2022-05-30 19:57:08 +0800

    fix register

--- src/pages/Register/RegisterForm.js
+++ src/pages/Register/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import WizardFormFirstPage from './WizardFormFirstPage';
 import WizardFormSecondPage from './WizardFormSecondPage';
