36d0461a21b29530287b9c00fc23e3307100361d
anca <annascasmawannn@gmail.com>
2022-06-02 19:39:49 +0800

    fix online profile

--- src/pages/Candidate/ProfileCandidate.js
+++ src/pages/Candidate/ProfileCandidate.js
@@ -1,80 +1,85 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProfileService from 'services/Profile/ProfileService';
+import utils from 'utils/utils';
 
 export default function ProfileCandidate() {
   const { id } = useParams();
-  const [dataProfile, setDataProfile] = useState({})
-  const [loading, setLoading] = useState(true)
+  const [dataProfile, setDataProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     async function fetchData() {
       const response_profile = await ProfileService.getOnlineProfileData(id);
-      setDataProfile(response_profile);
+      setDataProfile(response_profile.data.data);
       setLoading(false);
     }
     fetchData();
-    console.log(dataProfile)
-    
-  }, [])
-  
-
+  }, []);
 
   return (
-      <main className="flex lg:p-5 md:w-fit md:mx-auto shadow-mine">
-        <section className="bg-dark-blue py-10 sm:px-7 px-3 text-white ">
-          <div className="">
-            <img className="rounded-full lg:w-32 sm:w-24 w-16 mx-auto" src="/person.png" alt="" />
-            <div className="mt-5 flex items-center justify-center sm:gap-3 gap-1">
-              <p className="sm:text-lg text-sm">Rahmat Hidayat</p>
-              <svg
-                className="h-5 w-5"
-                width="28"
-                height="28"
-                viewBox="0 0 32 32"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12.8023 2.2071C14.1416 -0.392601 17.8586 -0.392601 19.1979 2.2071C20.0028 3.76922 21.8183 4.52127 23.4919 3.98575C26.2773 3.09457 28.9056 5.72285 28.0144 8.50818C27.4789 10.1818 28.2309 11.9975 29.7931 12.8023C32.3928 14.1416 32.3928 17.8586 29.7931 19.1979C28.2309 20.0028 27.4789 21.8184 28.0144 23.4919C28.9056 26.2773 26.2773 28.9056 23.4919 28.0144C21.8184 27.4789 20.0028 28.2309 19.1979 29.7931C17.8586 32.3928 14.1416 32.3928 12.8023 29.7931C11.9975 28.2309 10.1818 27.4789 8.50818 28.0144C5.72285 28.9056 3.09457 26.2773 3.98575 23.4919C4.52127 21.8184 3.76922 20.0028 2.2071 19.1979C-0.392601 17.8586 -0.392601 14.1416 2.2071 12.8023C3.76922 11.9975 4.52127 10.1818 3.98575 8.50818C3.09457 5.72285 5.72285 3.09457 8.50818 3.98575C10.1818 4.52127 11.9975 3.76922 12.8023 2.2071Z"
-                  fill="#00A3FF"
-                />
-                <path
-                  d="M20.7607 11.3173C21.1012 10.9165 21.6287 10.8976 22.0293 11.2383C22.4302 11.5789 22.3717 12.1073 22.031 12.5081L15.7718 20.4265C15.4258 20.8336 14.8128 20.8765 14.4135 20.5215L10.1278 16.712C9.73464 16.3625 9.69924 15.7606 10.0487 15.3675C10.3981 14.9743 11.0001 14.939 11.3932 15.2883L14.9512 18.451L20.7607 11.3173Z"
-                  fill="white"
-                />
-              </svg>
+    <main className="flex shadow-mine md:mx-auto md:w-fit lg:p-5">
+      {loading && <div>Loading...</div>}
+      {!loading && (
+        <>
+          <section className="bg-dark-blue py-10 px-3 text-white sm:px-7 ">
+            <div className="">
+              <img
+                className="mx-auto w-16 rounded-full sm:w-24 lg:w-32"
+                src="/person.png"
+                alt=""
+              />
+              <div className="mt-5 flex items-center justify-center gap-1 sm:gap-3">
+                <p className="text-sm sm:text-lg">{utils.makeCapital(dataProfile.full_name)}</p>
+                <svg
+                  className="h-5 w-5"
+                  width="28"
+                  height="28"
+                  viewBox="0 0 32 32"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M12.8023 2.2071C14.1416 -0.392601 17.8586 -0.392601 19.1979 2.2071C20.0028 3.76922 21.8183 4.52127 23.4919 3.98575C26.2773 3.09457 28.9056 5.72285 28.0144 8.50818C27.4789 10.1818 28.2309 11.9975 29.7931 12.8023C32.3928 14.1416 32.3928 17.8586 29.7931 19.1979C28.2309 20.0028 27.4789 21.8184 28.0144 23.4919C28.9056 26.2773 26.2773 28.9056 23.4919 28.0144C21.8184 27.4789 20.0028 28.2309 19.1979 29.7931C17.8586 32.3928 14.1416 32.3928 12.8023 29.7931C11.9975 28.2309 10.1818 27.4789 8.50818 28.0144C5.72285 28.9056 3.09457 26.2773 3.98575 23.4919C4.52127 21.8184 3.76922 20.0028 2.2071 19.1979C-0.392601 17.8586 -0.392601 14.1416 2.2071 12.8023C3.76922 11.9975 4.52127 10.1818 3.98575 8.50818C3.09457 5.72285 5.72285 3.09457 8.50818 3.98575C10.1818 4.52127 11.9975 3.76922 12.8023 2.2071Z"
+                    fill="#00A3FF"
+                  />
+                  <path
+                    d="M20.7607 11.3173C21.1012 10.9165 21.6287 10.8976 22.0293 11.2383C22.4302 11.5789 22.3717 12.1073 22.031 12.5081L15.7718 20.4265C15.4258 20.8336 14.8128 20.8765 14.4135 20.5215L10.1278 16.712C9.73464 16.3625 9.69924 15.7606 10.0487 15.3675C10.3981 14.9743 11.0001 14.939 11.3932 15.2883L14.9512 18.451L20.7607 11.3173Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+              <p className="mt-1 text-center text-xs sm:text-sm">
+                Teknik Informatika
+              </p>
             </div>
-            <p className="mt-1 text-center sm:text-sm text-xs">Teknik Informatika</p>
-          </div>
-          <EmploymentStatus />
-          <BasicInformation />
-          <SkillInfomation />
-          <CertificationInformation />
-        </section>
-        <section className="lg:w-[500px] w-fit text-[#7E8299]">
-          <AboutMe></AboutMe>
-          <WorkExperience></WorkExperience>
-          <IntershipExperience></IntershipExperience>
-          <EducationHistory></EducationHistory>
-        </section>
-      </main>
-     
-
-    
+            <EmploymentStatus />
+            <BasicInformation data={dataProfile}/>
+            <SkillInfomation />
+            <CertificationInformation />
+          </section>
+          <section className="w-fit text-[#7E8299] lg:w-[500px]">
+            <AboutMe></AboutMe>
+            <WorkExperience></WorkExperience>
+            <IntershipExperience></IntershipExperience>
+            <EducationHistory></EducationHistory>
+          </section>
+        </>
+      )}
+    </main>
   );
 }
 function EmploymentStatus({}) {
   return (
-    <p className="my-3 mx-auto w-fit rounded-sm bg-[#E8FFF3] sm:px-5 px-2 py-1 text-[10px] text-[#50CD89] sm:text-sm text-xs">
+    <p className="my-3 mx-auto w-fit rounded-sm bg-[#E8FFF3] px-2 py-1 text-[10px] text-xs text-[#50CD89] sm:px-5 sm:text-sm">
       Bekerja
     </p>
   );
 }
 
-function BasicInformation({}) {
+function BasicInformation({data}) {
   return (
-    <section className="mt-10 sm:text-sm text-xs space-y-5">
-      <div className="flex items-center sm:gap-5 gap-2">
+    <section className="mt-10 space-y-5 text-xs sm:text-sm">
+      <div className="flex items-center gap-2 sm:gap-5">
         <svg
           className="h-4 w-3"
           width="22"
@@ -88,9 +93,9 @@ function BasicInformation({}) {
             fill="white"
           />
         </svg>
-        <p>Makassar</p>
+        <p>{data.city_name}</p>
       </div>
-      <div className="flex items-center sm:gap-5 gap-2">
+      <div className="flex items-center gap-2 sm:gap-5">
         <svg
           className="h-4 w-4"
           width="22"
@@ -105,9 +110,9 @@ function BasicInformation({}) {
           />
         </svg>
 
-        <p>rahmathid@gmail.com</p>
+        <p>{data.phone_number}</p>
       </div>
-      <div className="flex items-center sm:gap-5 gap-2">
+      <div className="flex items-center gap-2 sm:gap-5">
         <svg
           className="h-4 w-4"
           width="8"
@@ -129,9 +134,9 @@ function BasicInformation({}) {
           />
         </svg>
 
-        <p>0850-8368-8368</p>
+        <p>{data.email}</p>
       </div>
-      <div className="flex items-center sm:gap-5 gap-2">
+      <div className="flex items-center gap-2 sm:gap-5">
         <svg
           className="h-4 w-4"
           width="27"
@@ -146,7 +151,7 @@ function BasicInformation({}) {
           />
         </svg>
 
-        <p>Rahmat</p>
+        <p>{data.url_linkedin ? data.url_linkedin : ''}</p>
       </div>
     </section>
   );
@@ -155,8 +160,10 @@ function BasicInformation({}) {
 function SkillInfomation({}) {
   return (
     <section className="mt-20">
-      <p className="sm:mb-6 mb-2  text-left sm:text-xl text-sm font-semibold">Skills</p>
-      <div className="text-left sm:text-sm text-xs sm:space-y-3 space-y-1">
+      <p className="mb-2 text-left  text-sm font-semibold sm:mb-6 sm:text-xl">
+        Skills
+      </p>
+      <div className="space-y-1 text-left text-xs sm:space-y-3 sm:text-sm">
         <p>Akuntansi</p>
         <p>Coding</p>
         <p>Marketing</p>
@@ -167,23 +174,23 @@ function SkillInfomation({}) {
 
 function CertificationInformation({}) {
   return (
-    <section className="sm:mt-20 mt-10">
-      <p className="sm:mb-6 mb-2 text-left sm:text-xl text-sm font-semibold">
+    <section className="mt-10 sm:mt-20">
+      <p className="mb-2 text-left text-sm font-semibold sm:mb-6 sm:text-xl">
         Award & Certification
       </p>
-      <div className="pr-10 text-left sm:text-sm text-xs sm:space-y-3 space-y-1 sm:w-[240px]">
+      <div className="space-y-1 pr-10 text-left text-xs sm:w-[240px] sm:space-y-3 sm:text-sm">
         <div>
           <p>Most Outstanding Employee of the Year (2015)</p>
           <div className="flex items-center gap-4">
             <hr className="w-2 border-b-[0.5px] border-white" />
-            <p className="text-xs mt-1">Pixelpoint Hive</p>
+            <p className="mt-1 text-xs">Pixelpoint Hive</p>
           </div>
         </div>
         <div>
           <p>Most Outstanding Employee of the Year (2015)</p>
           <div className="flex items-center gap-4">
             <hr className="w-2 border-b-[0.5px] border-white" />
-            <p className="text-xs mt-1">Pixelpoint Hive</p>
+            <p className="mt-1 text-xs">Pixelpoint Hive</p>
           </div>
         </div>
       </div>
@@ -194,8 +201,8 @@ function CertificationInformation({}) {
 function AboutMe({}) {
   return (
     <article className="mt-10">
-      <InformationTitle  title={"About Me"}/>
-      <p className="my-6 sm:pl-8 px-3 text-left md:text-sm text-xs md:w-[500px] w-fit">
+      <InformationTitle title={'About Me'} />
+      <p className="my-6 w-fit px-3 text-left text-xs sm:pl-8 md:w-[500px] md:text-sm">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod expedita
         ut, est voluptate voluptatibus nulla doloremque temporibus obcaecati,
         deserunt accusantium ad fuga aspernatur! Libero culpa, velit illum nulla
@@ -208,24 +215,28 @@ function AboutMe({}) {
 function WorkExperience({}) {
   return (
     <article>
-    <InformationTitle title={"Work Experience"}/>
-      <div className='sm:pl-8 px-3'>
-        <div className="my-8 text-left md:text-sm text-xs w-fit">
-          <p className="sm:text-xl text-base text-[#3F4254]">Junior React Developer</p>
+      <InformationTitle title={'Work Experience'} />
+      <div className="px-3 sm:pl-8">
+        <div className="my-8 w-fit text-left text-xs md:text-sm">
+          <p className="text-base text-[#3F4254] sm:text-xl">
+            Junior React Developer
+          </p>
           <p className="my-1 text-[#3F4254]">PT. Maju Jaya, 2010</p>
           <p>
-            First, a disclaimer – the entire process of writing a blog post often
-            takes more than a couple of hours, even if you can type eighty words
-            as per minute and your writing skills are sharp.
+            First, a disclaimer – the entire process of writing a blog post
+            often takes more than a couple of hours, even if you can type eighty
+            words as per minute and your writing skills are sharp.
           </p>
         </div>
-        <div className="my-8 text-left md:text-sm text-xs w-fit">
-          <p className="sm:text-xl text-base text-[#3F4254]">Junior React Developer</p>
+        <div className="my-8 w-fit text-left text-xs md:text-sm">
+          <p className="text-base text-[#3F4254] sm:text-xl">
+            Junior React Developer
+          </p>
           <p className="my-1 text-[#3F4254]">PT. Maju Jaya, 2010</p>
           <p>
-            First, a disclaimer – the entire process of writing a blog post often
-            takes more than a couple of hours, even if you can type eighty words
-            as per minute and your writing skills are sharp.
+            First, a disclaimer – the entire process of writing a blog post
+            often takes more than a couple of hours, even if you can type eighty
+            words as per minute and your writing skills are sharp.
           </p>
         </div>
       </div>
@@ -236,24 +247,28 @@ function WorkExperience({}) {
 function IntershipExperience({}) {
   return (
     <article>
-    <InformationTitle title={"Intern Experience"}/>
-      <div className='sm:pl-8 px-3'>
-        <div className="my-8 text-left md:text-sm text-xs w-fit">
-          <p className="sm:text-xl text-base text-[#3F4254]">Junior React Developer</p>
+      <InformationTitle title={'Intern Experience'} />
+      <div className="px-3 sm:pl-8">
+        <div className="my-8 w-fit text-left text-xs md:text-sm">
+          <p className="text-base text-[#3F4254] sm:text-xl">
+            Junior React Developer
+          </p>
           <p className="my-1 text-[#3F4254]">PT. Maju Jaya, 2010</p>
           <p>
-            First, a disclaimer – the entire process of writing a blog post often
-            takes more than a couple of hours, even if you can type eighty words
-            as per minute and your writing skills are sharp.
+            First, a disclaimer – the entire process of writing a blog post
+            often takes more than a couple of hours, even if you can type eighty
+            words as per minute and your writing skills are sharp.
           </p>
         </div>
-        <div className="my-8 text-left md:text-sm text-xs w-fit">
-          <p className="sm:text-xl text-base text-[#3F4254]">Junior React Developer</p>
+        <div className="my-8 w-fit text-left text-xs md:text-sm">
+          <p className="text-base text-[#3F4254] sm:text-xl">
+            Junior React Developer
+          </p>
           <p className="my-1 text-[#3F4254]">PT. Maju Jaya, 2010</p>
           <p>
-            First, a disclaimer – the entire process of writing a blog post often
-            takes more than a couple of hours, even if you can type eighty words
-            as per minute and your writing skills are sharp.
+            First, a disclaimer – the entire process of writing a blog post
+            often takes more than a couple of hours, even if you can type eighty
+            words as per minute and your writing skills are sharp.
           </p>
         </div>
       </div>
@@ -264,15 +279,17 @@ function IntershipExperience({}) {
 function EducationHistory({}) {
   return (
     <article>
-    <InformationTitle title={"Education History"}/>
-      <div className='sm:pl-8 px-3'>
-        <div className="my-8 text-left md:text-sm text-xs w-fit">
-          <p className="sm:text-xl text-base text-[#3F4254]">Junior React Developer</p>
+      <InformationTitle title={'Education History'} />
+      <div className="px-3 sm:pl-8">
+        <div className="my-8 w-fit text-left text-xs md:text-sm">
+          <p className="text-base text-[#3F4254] sm:text-xl">
+            Junior React Developer
+          </p>
           <p className="my-1 text-[#3F4254]">PT. Maju Jaya, 2010</p>
           <p>
-            First, a disclaimer – the entire process of writing a blog post often
-            takes more than a couple of hours, even if you can type eighty words
-            as per minute and your writing skills are sharp.
+            First, a disclaimer – the entire process of writing a blog post
+            often takes more than a couple of hours, even if you can type eighty
+            words as per minute and your writing skills are sharp.
           </p>
         </div>
       </div>
@@ -280,11 +297,12 @@ function EducationHistory({}) {
   );
 }
 
-    function InformationTitle({title}) {
-      return (<div className="bg-[#7E8299] py-4">
-        <p className="pl-8 text-left md:text-xl text-sm font-semibold text-white">
-         {title}
-        </p>
-      </div>);
-    }
-  
+function InformationTitle({ title }) {
+  return (
+    <div className="bg-[#7E8299] py-4">
+      <p className="pl-8 text-left text-sm font-semibold text-white md:text-xl">
+        {title}
+      </p>
+    </div>
+  );
+}
