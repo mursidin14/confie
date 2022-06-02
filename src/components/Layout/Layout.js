0e814a9ef22d32549b73d4e268c133bf9272f462
anca <annascasmawannn@gmail.com>
2022-06-02 19:04:04 +0800

    global data

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from 'components/Layout/Header';
 import ASideBar from 'components/Aside/AsideBar';
 import ASideBarMobile from 'components/Aside/ASideBarMobile';
+import ProfileService from 'services/Profile/ProfileService';
+
 export default function Layout({PageName, children, userId}) {
   const [offCanvas, setOffCanvas] = useState(false);
-
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      const response_profile = await ProfileService.getProfileData();
+      if(response_profile.data.meta.status == 'error'){
+        window.location.href = '/';
+      }
+      setData(response_profile);
+      setLoading(false);
+    }
+    fetchData();
+    console.log(data, 'layout')
+  }, []);
+  
   function handleNav() {
-    
     setOffCanvas(!offCanvas);
   }
   return (
