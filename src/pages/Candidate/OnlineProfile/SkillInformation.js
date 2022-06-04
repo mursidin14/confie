56c430ae5845367ef4841b5177d803322d45a416
anca <annascasmawannn@gmail.com>
2022-06-05 00:13:01 +0800

    fix: skill in online profile

--- src/pages/Candidate/OnlineProfile/SkillInformation.js
+++ src/pages/Candidate/OnlineProfile/SkillInformation.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 export default function SkillInfomation({ data }) {
+  console.log(data, 'skills')
     return (
       <section className="mt-20">
         <p className="mb-2 text-left  text-sm font-semibold sm:mb-6 sm:text-xl">
           Skills
         </p>
         <div className="space-y-1 text-left text-xs sm:space-y-3 sm:text-sm">
-          {data.map((skill, index) => (
+          {/* {data.map((skill, index) => (
             <p>{skill}</p>
-          ))}
+          ))} */}
         </div>
       </section>
     );
