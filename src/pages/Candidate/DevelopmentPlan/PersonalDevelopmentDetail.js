8a82c0ab5ec989791258b0879f3f555a1070a41d
anca <annascasmawannn@gmail.com>
2022-06-04 22:55:06 +0800

    add modal for update milestone

--- src/pages/Candidate/DevelopmentPlan/PersonalDevelopmentDetail.js
+++ src/pages/Candidate/DevelopmentPlan/PersonalDevelopmentDetail.js
@@ -5,6 +5,7 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { useParams } from 'react-router-dom';
 import ModalMilestone from 'components/Modal/ModalMilestone';
+import UpdateMilestone from 'components/ModalUpdate/UpdateMilestone';
 import PersonalPlanService from 'services/PersonalPlan/PersonalPlan';
 
 export default function PersonalDevelopmentDetail() {
@@ -17,18 +18,6 @@ export default function PersonalDevelopmentDetail() {
 
   useEffect(() => {
     async function getTargetDetail() {
-      // let response = await fetch(
-      //   `https://6267fd9b01dab900f1c82b3d.mockapi.io/target/${idDetail}/target_milestone`
-      // );
-      // let data = await response.json();
-      // let res = await fetch(
-      //   `https://6267fd9b01dab900f1c82b3d.mockapi.io/target/${idDetail}`
-      // );
-      // let dataTarget = await res.json();
-      // setTarget(dataTarget);
-      // setProgress(dataTarget.progress);
-      // setTargetMilestone(data);
-
       const response = await PersonalPlanService.getDetailPersonalPlanData(
         idDetail
       );
@@ -40,29 +29,13 @@ export default function PersonalDevelopmentDetail() {
     getTargetDetail();
   }, []);
 
-  function handleClick() {
-    setModal(!modal);
-  }
-  function handleChange(e) {
+  async function handleChange(e) {
     let target_checkbox = document.querySelectorAll('.target_checkbox').length;
     let checkbox_checked = document.querySelectorAll(
       '.target_checkbox:checked'
     ).length;
     let percentage = (checkbox_checked / target_checkbox) * 100;
     setProgress(percentage);
-    // async function updateTarget() {
-    //     let res = await fetch(`https://6267fd9b01dab900f1c82b3d.mockapi.io/target/${idDetail}`, {
-    //         method: 'PUT',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-    //             ...target,
-    //             progress: percentage
-    //         })
-    //     })
-    // }
-    // updateTarget();
   }
   return (
     <Layout userId={id} PageName={'Personal Development Plan'}>
@@ -135,7 +108,6 @@ export default function PersonalDevelopmentDetail() {
                         target_milestone={target_title}
                         quarter={quarter}
                         id={id}
-                        idDetail={idDetail}
                         handleChange={handleChange}
                       />
                     </>
@@ -155,7 +127,6 @@ export default function PersonalDevelopmentDetail() {
                         target_milestone={target_title}
                         quarter={quarter}
                         id={id}
-                        idDetail={idDetail}
                         handleChange={handleChange}
                       />
                     </>
@@ -175,7 +146,6 @@ export default function PersonalDevelopmentDetail() {
                         target_milestone={target_title}
                         quarter={quarter}
                         id={id}
-                        idDetail={idDetail}
                         handleChange={handleChange}
                       />
                     </>
@@ -195,7 +165,6 @@ export default function PersonalDevelopmentDetail() {
                         target_milestone={target_title}
                         quarter={quarter}
                         id={id}
-                        idDetail={idDetail}
                         handleChange={handleChange}
                       />
                     </>
@@ -212,9 +181,6 @@ export default function PersonalDevelopmentDetail() {
 
 function MilestoneTarget({
   target_milestone,
-  quarter,
-  idDetail,
-  id,
   handleChange,
 }) {
   return (
@@ -230,44 +196,8 @@ function MilestoneTarget({
           <p>{target_milestone}</p>
         </div>
         <div className="flex justify-center gap-2">
-          <a>
-            <svg
-              className="w-11"
-              width="34"
-              height="34"
-              viewBox="0 0 34 34"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 4.225C0 1.8916 1.8916 0 4.225 0H29.775C32.1084 0 34 1.8916 34 4.225V29.775C34 32.1084 32.1084 34 29.775 34H4.225C1.8916 34 0 32.1084 0 29.775V4.225Z"
-                fill="#FFF8DD"
-              />
-              <path
-                opacity="0.3"
-                d="M25.05 14.2649L23.4307 15.8833L19.1137 11.5663L20.7322 9.94705C21.0186 9.66076 21.4069 9.5 21.8119 9.5C22.2168 9.5 22.6051 9.66076 22.8915 9.94705L25.05 12.1056C25.3363 12.3919 25.4971 12.7803 25.4971 13.1852C25.4971 13.5901 25.3363 13.9785 25.05 14.2649ZM11.7652 24.4491L16.4152 22.8988L12.0982 18.5818L10.548 23.2318C10.4911 23.4016 10.4828 23.5841 10.5239 23.7584C10.565 23.9328 10.654 24.0922 10.7808 24.2187C10.9077 24.3451 11.0673 24.4337 11.2417 24.4744C11.4162 24.5152 11.5985 24.5064 11.7682 24.4491H11.7652Z"
-                fill="#FE9A00"
-              />
-              <path
-                d="M13.1805 23.975L11.769 24.446C11.5995 24.5024 11.4175 24.5106 11.2436 24.4695C11.0697 24.4284 10.9106 24.3398 10.7842 24.2134C10.6578 24.0871 10.5691 23.9281 10.5279 23.7542C10.4867 23.5803 10.4947 23.3984 10.551 23.2288L11.022 21.8165L13.1805 23.975ZM12.1013 18.5787L16.4183 22.8957L23.4338 15.8802L19.1168 11.5632L12.1013 18.5787Z"
-                fill="#FE9A00"
-              />
-            </svg>
-          </a>
-
+          <UpdateMilestone></UpdateMilestone>
           <button
-            onClick={async function () {
-              const res = await fetch(
-                `https://6267fd9b01dab900f1c82b3d.mockapi.io/target/${idDetail}/target_milestone/${id}`,
-                {
-                  method: 'DELETE',
-                  headers: {
-                    'Content-Type': 'application/json',
-                  },
-                }
-              );
-              window.location.reload();
-            }}
           >
             <svg
               className="w-11"
