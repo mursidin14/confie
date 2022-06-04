af0d848a2260fe3e4298b8237f2692308c618173
anca <annascasmawannn@gmail.com>
2022-06-04 23:55:53 +0800

    make separate component for online profile

---
+++ src/pages/Candidate/OnlineProfile/EmploymentStatus.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmploymentStatus({}) {
+    return (
+      <p className="my-3 mx-auto w-fit rounded-sm bg-[#E8FFF3] px-2 py-1 text-[10px] text-xs text-[#50CD89] sm:px-5 sm:text-sm">
+        Bekerja
+      </p>
+    );
+  }
