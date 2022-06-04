7e2e2aea69529d70425e3752e39072419ac3b9e9
anca <annascasmawannn@gmail.com>
2022-06-04 14:03:51 +0800

    fix

--- src/components/ModalUpdate/UpdateWorkExperience.js
+++ src/components/ModalUpdate/UpdateWorkExperience.js
@@ -3,7 +3,12 @@ import { Dialog, Transition } from '@headlessui/react';
 import { Fragment, useState } from 'react';
 import ProfileService from 'services/Profile/ProfileService';
 import utils from 'utils/utils';
-export default function ModalWorkExperience() {
+export default function UpdateWorkExperience({ id }) {
+  let [isOpen, setIsOpen] = useState(false);
+  const [dataWorkExperience, setDataWorkExperience] = useState({
+    is_current: false,
+    status: 'onsite',
+  });
   const [error, setError] = useState([]);
   function closeModal() {
     setIsOpen(false);
@@ -26,7 +31,7 @@ export default function ModalWorkExperience() {
     let data = {
       ...dataWorkExperience,
     };
-    const response = await ProfileService.addJobExperience(data);
+    const response = await ProfileService.updateJobExperience(id, data);
     if (response.data.meta.status == 'error') {
       let errors = [];
       let error = response.data.data;
@@ -75,12 +80,29 @@ export default function ModalWorkExperience() {
   return (
     <>
       <div className="flex items-center justify-center">
-        <button
-          type="button"
-          onClick={openModal}
-          className="rounded-md bg-[#FE9A00] px-5 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-        >
-          Tambah
+        <button onClick={openModal}>
+          <svg
+            className="w-11"
+            width="34"
+            height="34"
+            viewBox="0 0 34 34"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M0 4.225C0 1.8916 1.8916 0 4.225 0H29.775C32.1084 0 34 1.8916 34 4.225V29.775C34 32.1084 32.1084 34 29.775 34H4.225C1.8916 34 0 32.1084 0 29.775V4.225Z"
+              fill="#FFF8DD"
+            />
+            <path
+              opacity="0.3"
+              d="M25.05 14.2649L23.4307 15.8833L19.1137 11.5663L20.7322 9.94705C21.0186 9.66076 21.4069 9.5 21.8119 9.5C22.2168 9.5 22.6051 9.66076 22.8915 9.94705L25.05 12.1056C25.3363 12.3919 25.4971 12.7803 25.4971 13.1852C25.4971 13.5901 25.3363 13.9785 25.05 14.2649ZM11.7652 24.4491L16.4152 22.8988L12.0982 18.5818L10.548 23.2318C10.4911 23.4016 10.4828 23.5841 10.5239 23.7584C10.565 23.9328 10.654 24.0922 10.7808 24.2187C10.9077 24.3451 11.0673 24.4337 11.2417 24.4744C11.4162 24.5152 11.5985 24.5064 11.7682 24.4491H11.7652Z"
+              fill="#FE9A00"
+            />
+            <path
+              d="M13.1805 23.975L11.769 24.446C11.5995 24.5024 11.4175 24.5106 11.2436 24.4695C11.0697 24.4284 10.9106 24.3398 10.7842 24.2134C10.6578 24.0871 10.5691 23.9281 10.5279 23.7542C10.4867 23.5803 10.4947 23.3984 10.551 23.2288L11.022 21.8165L13.1805 23.975ZM12.1013 18.5787L16.4183 22.8957L23.4338 15.8802L19.1168 11.5632L12.1013 18.5787Z"
+              fill="#FE9A00"
+            />
+          </svg>
         </button>
       </div>
 
@@ -112,7 +134,7 @@ export default function ModalWorkExperience() {
                 <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <div className="flex items-center justify-between px-8">
                     <h3 className="text-base font-semibold ">
-                      Tambah Pengalaman Kerja
+                      Edit Pengalaman Kerja
                     </h3>
                   </div>
                   <hr className=" my-2 w-full border-b-[1px] border-[#3F4254]/10" />
@@ -157,7 +179,7 @@ export default function ModalWorkExperience() {
                         </div>
                         <div className="lg:w-7/12">
                           <select
-                            className="w-full h-fit bg-soft-gray rounded-md p-5 text-sm"
+                            className="h-fit w-full rounded-md bg-soft-gray p-5 text-sm"
                             name="status"
                             id="status"
                             onChange={(e) => {
@@ -208,7 +230,7 @@ export default function ModalWorkExperience() {
                       onClick={handleSubmit}
                       className="rounded-md bg-[#FE9A00] px-4 py-2 text-sm text-white"
                     >
-                      Submit
+                      Edit
                     </button>
                   </div>
                 </Dialog.Panel>
