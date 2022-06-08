e5d667aa3ff343f6b9cbe00c3b7aa8d7edc4fadd
anca <annascasmawannn@gmail.com>
2022-06-08 15:36:40 +0800

    fix: not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,9 +6,11 @@ export default function NotFound() {
   return (
     <>
         <div className='flex justify-center items-center min-h-[calc(100vh-11vh)]'>
+          <div>
           <p className='text-9xl mb-5 font-bold'>4😓4</p>
           <p>Halaman ga ada, bang!</p>
           <a className='text-xs underline font-bold' href={`/dashboard/${id}`}>Back to Home</a>
+          </div>
         </div>
     </>
   )
