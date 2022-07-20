932a1911f96486c0db61b36703995bec1f651b3e
anca <83209348+aancaa@users.noreply.github.com>
2022-07-20 17:45:29 +0800

    fix: layout login

--- src/components/Aside/AsideLogin.js
+++ src/components/Aside/AsideLogin.js
@@ -3,12 +3,12 @@ export default function AsideLogin() {
 
   return (
     <section className="gradient-login text-white lg:w-7/12 hidden lg:flex flex-col justify-center">
-      <div className="text-left relative left-10 bottom-12">
+      <div className="text-left relative pl-10 bottom-12">
       <img className="w-60 mb-7" src="/logo-login.png" alt="" />
-        <h1 className="block lg:w-[600px] text-sm sm:text-4xl leading-3">
+        <h1 className="block text-sm sm:text-[2.5rem] leading-[3.2rem] tracking-[1.5px]">
           Menemukan Pekerjaan Impian Jadi Lebih Mudah!
         </h1>
-        <p className="sm:mt-3 lg:w-10/12 text-sm sm:text-base">
+        <p className="sm:mt-[1.2rem] lg:w-8/12 text-[1.2rem] leading-[1.8rem] tracking-[0.3px] sm:text-base">
         Kembangkan potensi terbaikmu dan bergabung bersama kami di platform
 pilihan pertama yang akan membantu kamu membangun karir impianmu!
         </p>
